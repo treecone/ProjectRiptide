859dc5385bf948a9a0f2ec7b650ec4db580c53e1
Mira <mla4783@rit.edu>
2019-03-24 16:44:55 -0400

    Main Menu and Pause Menu created
    Volume slider and notification buttons do nothing at the moment, will set something up when we add music etc.

---
+++ Project Riptide/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    //loads scene by index
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
