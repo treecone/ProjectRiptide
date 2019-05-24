18ddc5932dde097a5e70501971db0a6abe07ef37
SeanLynch717 <sdlgamerguy@gmail.com>
2019-05-24 17:51:28 -0400

    Movement
    Turning has some resistance now.

--- Project Riptide/Assets/Scripts/PlayerController.cs
+++ Project Riptide/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,23 @@ public class PlayerController : MonoBehaviour
 	private InputManager inputManager; 
 	private Rigidbody rb;
 
-	public Vector3 touchMove;
 	public float speed = 1f;
-	public float rotationSpeed = 0.5f;
+    public float rotationSpeed = 0f;
     public float targetVelocity;
-    private float currentVelocity;
+    public float currentVelocity;
+    private float totalRotation;
+    private float currentRotation;
+    private float turningTime = 0f;
+    private float turnSpeed = .001f;
+
+    private Quaternion from;
+    private Quaternion to;
 
 	private Camera camera;
 	private Vector3 newPosition;
 
     //The vectors defined by the points current touch position and touch visuals position.
-    public Vector2 previousTouchVector;
+    public Vector2 startTouchVector;
     public Vector2 currentTouchVector;
 
 	// Start is called before the first frame update
@@ -26,58 +32,78 @@ public class PlayerController : MonoBehaviour
         inputManager = GameObject.Find("Canvas").GetComponent<InputManager>();
 		rb = GetComponent<Rigidbody>();
 		camera = Camera.main;
-        previousTouchVector = Vector3.zero;
-        currentTouchVector = Vector3.zero;
+        startTouchVector = Vector2.zero;
+        currentTouchVector = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If the player is swiping.
         if(inputManager.currentlySwiping)
 		{
-			Vector2 touchPos = inputManager.currentTouchPos - inputManager.startTouchPos;
-			touchMove = new Vector3(-touchPos.y, 0, touchPos.x);	//Convert 2D input into 3d movement
-			touchMove.Normalize();
             //Set the current vector now.
             currentTouchVector = inputManager.currentTouchPos - (Vector2)(inputManager.touchVisual.GetComponent<RectTransform>().anchoredPosition);
+            //If this is the first frame of touching, set the start touch vector.
+            if(startTouchVector == Vector2.zero)
+            {
+                startTouchVector = currentTouchVector;
+            }
             //Only update velocity if the player hasn't made the touch vector shorter than what it began as.
             if(currentTouchVector.magnitude>=100)
             {
+                //Set the target velocity.
                 targetVelocity = currentTouchVector.magnitude/100f - 1f;
                 if(targetVelocity>6.6f)
                 {
                     targetVelocity = 6.6f;
                 }
-            }
-			//If you have moved your finger from the start position even slightly.
-            if(Mathf.Abs(touchPos.x) > 100f || Mathf.Abs(touchPos.y) > 100f)
-            {
-                //Rotate the ship.
-                float goalRotation = Vector2.SignedAngle(currentTouchVector,previousTouchVector);
-                transform.Rotate(0,goalRotation/1,0,Space.Self);
-                //Change the ships velocity.   
-                if(rb.velocity.magnitude < targetVelocity - .1f)
+                //The total rotation needed.
+                totalRotation = Vector2.SignedAngle(currentTouchVector,startTouchVector);
+                //Rotate the boat.
+                transform.Rotate(0, rotationSpeed, 0, Space.Self);
+                //Increase the current anglular distance traveled by the boat.
+                currentRotation += rotationSpeed;
+                //determine the sign of rotation.
+                float sign = (totalRotation > 0)? 1f : -1f;
+                //Increase the speed of rotation.
+                rotationSpeed += .002f * sign;             
+                //The ship has completed its rotation, so reset the neccessary components.
+                if(Mathf.Abs(currentRotation) >= Mathf.Abs(totalRotation))
                 {
-                    currentVelocity += .01f;
+                    startTouchVector = currentTouchVector;
+                    currentRotation = 0;
+                    totalRotation = 0;
+                    rotationSpeed = 0;
                 }
-                else if(rb.velocity.magnitude > targetVelocity +.1f)
-                {
-                    currentVelocity -= .01f;
-                }    
-                rb.velocity = transform.forward * currentVelocity;
+
+
+                /*turningTime += Time.deltaTime * turnSpeed;
+                float yRot = transform.rotation.ToEuler().y;
+                Vector2 currentRotation = new Vector2(Mathf.Cos(yRot)*-1f, Mathf.Sin(yRot));
+                float goalRotation = Vector2.SignedAngle(currentTouchVector,currentRotation) * Mathf.Rad2Deg; // How much the ship needs to turn total.
+                from = transform.rotation;
+                to = Quaternion.AngleAxis(goalRotation, Vector3.up) * from;
+                transform.rotation = Quaternion.Lerp(from, to , turningTime);
+                */
             }
+			
         }
+        //If the player is not swiping reset the touch vectors.
         else
         {
-            currentTouchVector = Vector3.zero;
+            currentTouchVector = Vector2.zero;
+            startTouchVector = Vector2.zero;
         }
-	    previousTouchVector = currentTouchVector;
-		/*else if(Input.GetMouseButtonDown(0))
-		{
-			Vector3 clickedPos = Camera.main.ViewportToWorldPoint(Input.mousePosition);
-			print(clickedPos.ToString());
-			//clickedPos = new Vector2(clickedPos.x, 0, clickedPos.y);
-			//transform.position = clickedPos;
-		}*/
+        //Update the ships velocity according to what its target velocity is set at.   
+        if(rb.velocity.magnitude < targetVelocity - .1f)
+        {
+             currentVelocity += .01f;
+        }
+        else if(rb.velocity.magnitude > targetVelocity +.1f)
+        {
+             currentVelocity -= .01f;
+        }    
+        rb.velocity = transform.forward * currentVelocity;
 	}
 }
