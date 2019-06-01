f545720ef32e9ab060ff82d36d75fe0b986de8a4
Jimmie Harkins <ghana796@gmail.com>
2019-06-01 16:55:45 -0400

    Cannon fire stuff

---
+++ Project Riptide/Assets/Scripts/CannonBallBehaviorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallBehaviorScript : MonoBehaviour
+{
+    public int damageDealt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Enemy enemyHit = collision.gameObject.GetComponent<Enemy>();
+        if(enemyHit != null)
+        {
+            enemyHit.TakeDamage(damageDealt);
+            Debug.Log("hit");
+        }
+    }
+}
---
+++ Project Riptide/Assets/Scripts/CannonFireScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonFireScript : MonoBehaviour
+{
+    public GameObject cannonBall;
+    public float fireSpeedHoriz;
+    public float fireSpeedVert;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("a"))
+        {
+            Fire(true);
+        }
+        if(Input.GetKeyDown("d"))
+        {
+            Fire(false);
+        }
+    }
+
+    void Fire(bool side)
+    {
+        Vector3 direction = side ? -transform.right : transform.right;
+        GameObject ball = Instantiate(cannonBall, transform.position + (transform.localScale.x / 2) * direction, Quaternion.identity);
+        ball.GetComponent<Rigidbody>().velocity = direction * fireSpeedHoriz + transform.up * fireSpeedVert;
+    }
+}
