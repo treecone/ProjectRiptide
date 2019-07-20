871e83214090922a33de83f0543e6d87a44e4676
SeanLynch717 <sdlgamerguy@gmail.com>
2019-07-20 17:21:14 -0400

    visual indicator...
    ...of a speed increase/decrease.

--- Project Riptide/Assets/Scripts/ShipWaves.cs
+++ Project Riptide/Assets/Scripts/ShipWaves.cs
@@ -9,6 +9,12 @@ public class ShipWaves : MonoBehaviour
     private double timer;
     private GameObject ship;
 
+    private float currentVelocity;
+    private float previousVelocity;
+
+    public Material[] materials;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +25,24 @@ public class ShipWaves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        previousVelocity = ship.GetComponent<Rigidbody>().velocity.magnitude;
         timer += Time.deltaTime;
         if (timer > 1.5)
         {
+            //The ship is changing speed.
             if((ship.GetComponent<Rigidbody>().velocity.magnitude > .1 && ship.GetComponent<Rigidbody>().velocity.magnitude < 2.4f) ||
             (ship.GetComponent<Rigidbody>().velocity.magnitude > 2.6f && ship.GetComponent<Rigidbody>().velocity.magnitude < 4.9f)){
                 timer = 0;
                 waves.Add(Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.Euler(90, 0, 0)));
                 waves[waves.Count - 1].transform.position = GameObject.Find("Ship").transform.position - GameObject.Find("Ship").transform.TransformDirection(new Vector3(0, 0, 1)) + new Vector3(0, .3f, 0);
+                if (IsVelocityIncreasing())
+                {
+                    waves[waves.Count - 1].GetComponent<Renderer>().material = materials[1];
+                }
+                else
+                {
+                    waves[waves.Count - 1].GetComponent<Renderer>().material = materials[0];
+                }
                 if (waves.Count > 1)
                 {
                     Destroy(waves[0]);
@@ -55,5 +71,14 @@ public class ShipWaves : MonoBehaviour
             }
 
         }
+        currentVelocity = ship.GetComponent<Rigidbody>().velocity.magnitude;
+    }
+    /// <summary>
+    /// Gets whether or not the first derivative of the ships velocity is positive or negative.
+    /// </summary>
+    /// <returns> True if the first derivative of velocity is positive.</returns>
+    public bool IsVelocityIncreasing()
+    {
+        return currentVelocity > previousVelocity;
     }
 }
