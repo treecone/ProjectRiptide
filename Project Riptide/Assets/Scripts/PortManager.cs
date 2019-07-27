58cb9813a137d1ed669edea2f20ce812f86b8f86
Robert <42982719+Zytion@users.noreply.github.com>
2019-07-27 17:31:26 -0400

    Fixed up ports. Now ready for some functionality

--- Project Riptide/Assets/Scripts/PortManager.cs
+++ Project Riptide/Assets/Scripts/PortManager.cs
@@ -1,40 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PortManager : MonoBehaviour
 {
 	private GameObject player;
 	public GameObject PortUI;
-
-	/// <summary>
-	/// Used to give player time to move away from the port after exiting the port menu
-	/// </summary>
-	public float EscapeTimer {get; set; } 
+	public GameObject GameManager;
+	private PortUIManager UIManager; 
 
     // Start is called before the first frame update
     void Start()
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
-		EscapeTimer = 0;
-    }
+		UIManager = GameManager.GetComponent<PortUIManager>();
+	}
 
     // Update is called once per frame
     void Update()
     {
 		//If the escape timer is zero and the player is near the port, then disable player movement
-		if(EscapeTimer <= 0 && Vector3.Distance(player.transform.position, transform.position) < 0.5)
+		if (UIManager.EscapeTimer <= 0 && Vector3.Distance(player.transform.position, transform.position) < 1.5)
 		{
 			player.GetComponent<PlayerController>().enabled = false;
 			PortUI.SetActive(true);
-		}
-		else
-		{
-			player.GetComponent<PlayerController>().enabled = false;
-		}
-		if(EscapeTimer > 0)
-		{
-			EscapeTimer -= Time.deltaTime;
+			player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			player.transform.position = gameObject.transform.position;
 		}
 	}
 }
