f6710bb436c915eb1e473233b6e5a8d18a4beead
Skyler Zartman <zartsky8@gmail.com>
2020-01-01 16:29:47 -0600

    Updated water and inv

--- Project Riptide/Assets/Scripts/Inventory.cs
+++ Project Riptide/Assets/Scripts/Inventory.cs
@@ -150,31 +150,3 @@ public class Inventory : MonoBehaviour
         }
     }
 }
-
-public class ItemSlot
-{
-    public ItemSlot(Item item, int amount, GameObject theSlot)
-    {
-        this.item = item;
-        this.amount = amount;
-        this.theSlotObject = theSlot;
-    }
-
-    public void UpdateSlotVisuals () //Updates the image and amount text
-    {
-        theSlotObject.transform.Find("Item Image").GetComponent<Image>().sprite = item.sprite;
-        theSlotObject.transform.Find("AmountText").GetComponent<TextMeshProUGUI>().text = amount.ToString();
-    }
-
-    public void Clear ()
-    {
-        item = null;
-        amount = 0;
-        theSlotObject.transform.Find("Item Image").GetComponent<Image>().sprite = null;
-        theSlotObject.transform.Find("AmountText").GetComponent<TextMeshProUGUI>().text = "";
-    }
-
-    public Item item { get; set; }
-    public int amount { get; set; }
-    public GameObject theSlotObject { get; set; }
-}
--- Project Riptide/Assets/Scripts/itemSlot.cs
+++ Project Riptide/Assets/Scripts/itemSlot.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class itemSlot : MonoBehaviour
+public class ItemSlot
 {
-    // Start is called before the first frame update
-    void Start()
+    public ItemSlot(Item item, int amount, GameObject theSlot)
     {
-        
+        this.item = item;
+        this.amount = amount;
+        this.theSlotObject = theSlot;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateSlotVisuals() //Updates the image and amount text
     {
-        
+        theSlotObject.transform.Find("Item Image").GetComponent<Image>().sprite = item.sprite;
+        theSlotObject.transform.Find("AmountText").GetComponent<TextMeshProUGUI>().text = amount.ToString();
     }
+
+    public void Clear()
+    {
+        item = null;
+        amount = 0;
+        theSlotObject.transform.Find("Item Image").GetComponent<Image>().sprite = null;
+        theSlotObject.transform.Find("AmountText").GetComponent<TextMeshProUGUI>().text = "";
+    }
+
+    public Item item { get; set; }
+    public int amount { get; set; }
+    public GameObject theSlotObject { get; set; }
 }
