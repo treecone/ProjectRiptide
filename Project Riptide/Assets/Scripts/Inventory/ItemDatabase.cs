a09666226ef0759d386451e51d38e1002882c416
Skyler Zartman <zartsky8@gmail.com>
2020-05-18 14:00:54 -0500

    Tree shader and Droptable

--- Project Riptide/Assets/Scripts/Inventory/ItemDatabase.cs
+++ Project Riptide/Assets/Scripts/Inventory/ItemDatabase.cs
@@ -41,6 +41,7 @@ public class ItemDatabase : MonoBehaviour
             }
             else
             {
+                //Delete this?
                 database.Add(new Item((int)itemData[i]["id"], nameTempString, itemData[i]["description"].ToString(), (bool)itemData[i]["stackable"], (int)itemData[i]["rarity"], (int)itemData[i]["value"], itemData[i]["slug"].ToString(), Resources.Load<Sprite>("Inventory/ItemSprites/" + nameTempString + "Sprite"), (int)itemData[i]["amount"], (int)itemData[i]["maxAmount"]));
             }
         }
