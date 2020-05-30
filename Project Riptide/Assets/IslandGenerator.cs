95781a092a8b84d831658e932366d1b0d4f25c68
Jimmie Harkins <ghana796@gmail.com>
2020-05-30 16:44:23 -0400

    Added some more config options to island generation

--- Project Riptide/Assets/IslandGenerator.cs
+++ Project Riptide/Assets/IslandGenerator.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class IslandGenerator : MonoBehaviour
 {
+    [System.Serializable]
+    private class DecoObject
+    {
+        public GameObject gameObject;
+        public bool rotatesToGround;
+        public float verticalOffset;
+    }
     private Mesh _mesh;
     [SerializeField]
-    private List<GameObject> objectsToAdd;
+    private List<DecoObject> objectsToAdd;
     [SerializeField]
     private GameObject _water;
     // Start is called before the first frame update
@@ -32,7 +39,7 @@ public class IslandGenerator : MonoBehaviour
     {
         int numTris = _mesh.triangles.Length / 3;
         Vector3 result = new Vector3(0, _water.transform.position.y - 1, 0);
-
+        Vector3 normal = Vector3.zero;
         while(result.y <= _water.transform.position.y)
         {
             int randIndex = Random.Range(0, numTris);
@@ -42,7 +49,7 @@ public class IslandGenerator : MonoBehaviour
 
             Vector3 b1 = v2 - v1;
             Vector3 b2 = v3 - v1;
-
+            normal = Vector3.Cross(b1, b2).normalized;
             float r1 = Random.Range(0.0f, 1.0f);
             float r2 = Random.Range(0.0f, 1.0f);
             if (r1 + r2 > 1.0f)
@@ -54,11 +61,15 @@ public class IslandGenerator : MonoBehaviour
             result = v1 + r1 * b1 + r2 * b2;
             result = new Vector3(result.x * transform.localScale.x, result.y * transform.localScale.y, result.z * transform.localScale.z);
             result += transform.position;
-            
         }
-        Debug.DrawLine(result, result + Vector3.up, Color.red, 1.0f);
-        Instantiate(objectsToAdd[Random.Range(0, objectsToAdd.Count)], result, Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0));
-        Debug.Log(result);
+        DecoObject obj = objectsToAdd[Random.Range(0, objectsToAdd.Count)];
+        if(!obj.rotatesToGround)
+        {
+            normal = Vector3.up;
+        }
+        GameObject newObject = Instantiate(obj.gameObject, result, Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0));
+        newObject.transform.up = normal;
+        newObject.transform.position += normal * obj.verticalOffset;
         return result;
     }
 }
