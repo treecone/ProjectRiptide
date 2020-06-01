6237b27b161c5456aca63e0418f755728438b90a
SeanLynch717 <sdlgamerguy@gmail.com>
2020-06-01 15:08:37 -0400

    Enemies
    Enemies spawn in groups

--- Project Riptide/Assets/Scripts/Chunks/Chunk.cs
+++ Project Riptide/Assets/Scripts/Chunks/Chunk.cs
@@ -9,6 +9,11 @@ public class Chunk
     public GameObject chunk;
     public Region region;
     public Vector2 center;
+    public bool hasEnemies;
+    public bool hasMonster;
+
+    public List<GameObject> enemies = new List<GameObject>();
+    public bool[] enemieIsLoaded;
 
     /// <summary>
     /// Constructs a chunk object
@@ -16,10 +21,13 @@ public class Chunk
     /// <param name="chunk"> The chunk itself</param>
     /// <param name="region"> The name of the region</param>
     /// <param name="center"> Where the center of the chunk is located</param>
-    public Chunk(GameObject chunk, Region region, Vector2 center)
+    public Chunk(GameObject chunk, Region region, Vector2 center, List<GameObject> enemies)
     {
         this.chunk = chunk;
         this.region = region;
         this.center = center;
+        this.enemies = enemies;
+        if (enemies.Count > 0)
+            enemieIsLoaded = new bool[enemies.Count];
     }
 }
