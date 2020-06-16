34a25dc67cd8e5803d17cdef3187ab3bfe6293b3
mla4783 <mla4783@rit.edu>
2020-06-16 14:01:08 -0400

    Removed LeanTween plugin, added DOTween, worked on animating canvas

--- Project Riptide/Assets/Scripts/Inventory/UIAnimMethods.cs
+++ Project Riptide/Assets/Scripts/Inventory/UIAnimMethods.cs
@@ -1,21 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DentedPixel;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class UIAnimMethods : MonoBehaviour
 {
 
+    public void SlideTLeft(GameObject gObj)
+    {
+        gObj.transform.DOLocalMoveX(-1505f, .7f).SetUpdate(true);
+        Debug.Log("SlideL");
+    }
+
+    public void SlideTRight(GameObject gObj)
+    {
+        gObj.transform.DOLocalMoveX(0, .65f).SetUpdate(true);
+        Debug.Log("SlideR");
+    }
+
     public void SlideLeft(GameObject gObj)
     {
-        LeanTween.moveLocalX(gObj, 0, 1f);
+        gObj.transform.DOLocalMoveX(-1505f, .65f).SetUpdate(true);
         Debug.Log("SlideL");
     }
 
     public void SlideRight(GameObject gObj)
     {
-        LeanTween.moveLocalX(gObj, -1050f, 1f);
+        gObj.transform.DOLocalMoveX(0, .7f).SetUpdate(true);
         Debug.Log("SlideR");
+    }
+
+    public void SlideCLeft(GameObject gObj)
+    {
+        gObj.transform.DOLocalMoveX(-897f, .65f).SetUpdate(true);
+    }
+    public void SlideCRight(GameObject gObj)
+    {
+        gObj.transform.DOLocalMoveX(608f, .7f).SetUpdate(true);
     }
 
 }
