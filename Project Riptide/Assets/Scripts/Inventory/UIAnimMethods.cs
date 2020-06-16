1627d0638efb118a95ef578479783149b6ee145f
mla4783 <mla4783@rit.edu>
2020-06-16 14:26:36 -0400

    Fixed merging bug

--- Project Riptide/Assets/Scripts/Inventory/UIAnimMethods.cs
+++ Project Riptide/Assets/Scripts/Inventory/UIAnimMethods.cs
@@ -6,37 +6,27 @@ using UnityEngine.UI;
 
 public class UIAnimMethods : MonoBehaviour
 {
-    public void Start()
-    {
-        LeanTween.cancelAll();
-        LeanTween.reset();
-    }
-
+    //SLIDING TITLE IN AND OUT
     public void SlideTLeft(GameObject gObj)
     {
         gObj.transform.DOLocalMoveX(-1505f, .7f).SetUpdate(true);
-        Debug.Log("SlideL");
     }
-
     public void SlideTRight(GameObject gObj)
     {
         gObj.transform.DOLocalMoveX(0, .65f).SetUpdate(true);
-        Debug.Log("SlideR");
     }
 
+    //SLIDING BASE MATERIALS IN AND OUT
     public void SlideLeft(GameObject gObj)
     {
         gObj.transform.DOLocalMoveX(-1505f, .65f).SetUpdate(true);
-        Debug.Log("SlideL");
     }
-
     public void SlideRight(GameObject gObj)
     {
-        LeanTween.DOLocalMoveX(gObj, -1050f, 1f);
-        LeanTween.update();
-        Debug.Log("SlideR");
+        gObj.transform.DOLocalMoveX(0, .7f).SetUpdate(true);
     }
 
+    //SLIDING CLOSE BUTTON IN AND OUT
     public void SlideCLeft(GameObject gObj)
     {
         gObj.transform.DOLocalMoveX(-897f, .65f).SetUpdate(true);
@@ -45,5 +35,16 @@ public class UIAnimMethods : MonoBehaviour
     {
         gObj.transform.DOLocalMoveX(608f, .7f).SetUpdate(true);
     }
+
+    //SLIDING GOLD IN AND OUT
+    public void SlideGLeft(GameObject gObj)
+    {
+        gObj.transform.DOLocalMoveX(-1977f, .7f).SetUpdate(true);
+    }
+    public void SlideGRight(GameObject gObj)
+    {
+        gObj.transform.DOLocalMoveX(-472.2f, .65f).SetUpdate(true);
+    }
+
 
 }
