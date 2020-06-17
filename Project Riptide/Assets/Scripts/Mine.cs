9b8868a431c12ad162820ff6c8f12a6afc7b0de8
Jimmie Harkins <ghana796@gmail.com>
2020-06-17 17:11:36 -0400

    Added mines - inworld objects that can do damage or apply statuses on collision

---
+++ Project Riptide/Assets/Scripts/Mine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    private StatusEffects _playerStatusEffects;
+    private PlayerHealth _playerHealth;
+
+    [SerializeField]
+    private float damage;
+    [SerializeField]
+    private string statusType;
+    [SerializeField]
+    private float statusDuration;
+    [SerializeField]
+    private float statusLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerStatusEffects = _player.GetComponent<StatusEffects>();
+        _playerHealth = _player.GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            _playerHealth.TakeDamage(damage);
+            _playerStatusEffects.AddStatus(statusType, statusDuration, statusLevel);
+            Destroy(gameObject);
+        }
+    }
+}
