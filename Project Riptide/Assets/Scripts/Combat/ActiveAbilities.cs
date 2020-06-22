eb7557e717449503fb9bfb702bd17e79c6665148
MrMeansock <caden.messenger@gmail.com>
2020-06-22 09:32:17 -0400

    Fixed my scene

--- Project Riptide/Assets/Scripts/Combat/ActiveAbilities.cs
+++ Project Riptide/Assets/Scripts/Combat/ActiveAbilities.cs
@@ -175,7 +175,7 @@ public class ActiveAbilities : MonoBehaviour
     {
         //Applies force in direction of player velocity
         Vector3 netForce = _movementScript.GetVelocity().normalized;
-        netForce *= 500.0f;
+        netForce *= 9.0f / Time.deltaTime;
         _movementScript.ApplyForce(netForce);
         return true;
     }
