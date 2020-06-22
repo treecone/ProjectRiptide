62ebf0bd7a1eabdbdcf9e838b65f34d4d14ef3e1
Jimmie Harkins <ghana796@gmail.com>
2020-06-22 11:23:50 -0400

    Beginning of Lootbox

---
+++ Project Riptide/Assets/Scripts/Lootbox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lootbox : MonoBehaviour
+{
+    [SerializeField]
+    private List<Mesh> _meshes;
+    [SerializeField]
+    private Material _boxMaterial;
+
+    private MeshFilter _meshFilter;
+    private MeshRenderer _meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        SetShape();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetShape()
+    {
+        _meshRenderer.material = _boxMaterial;
+        _meshFilter.mesh = _meshes[Random.Range(0, _meshes.Count)];
+    }
+}
