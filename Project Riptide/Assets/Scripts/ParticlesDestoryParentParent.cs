96c237292dea5b7cfc4b754e4ab7dda92c796d5c
MrMeansock <caden.messenger@gmail.com>
2020-07-07 15:57:12 -0400

    Added new bubble broth particles

---
+++ Project Riptide/Assets/Scripts/ParticlesDestoryParentParent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesDestoryParentParent : MonoBehaviour
+{
+    /// <summary>
+    /// Destroy parent to particle system when particle finishes playing
+    /// </summary>
+    public void OnParticleSystemStopped()
+    {
+        Destroy(transform.parent.parent.gameObject);
+    }
+}
